68f0197ca845168f4285d730a73f91f42a74e1f3
Zhenya Buyak <12345678>
2021-07-05 11:21:20 +0300

    homework 3 vowels_arr

--- Homework_3/vowels_arr.js
+++ Homework_3/vowels_arr.js
@@ -12,16 +12,54 @@ const objLetters = {
     sum: 0,
 }
 
+/* ForEach */
 function countLetters(str){
     const bufArray = Array.from(str.toString().toLowerCase());
     bufArray.forEach((value) =>{
         if(value in objLetters){
-            objLetters[value]++;
             objLetters.sum++;
         }
     });
     return objLetters.sum;
 }
 
-alert('Согласных букв: ' + countLetters(prompt('Введите строку')));
+alert('Согласных букв: ' + countLetters(prompt('Введите строку (ForEach)')));
 console.log(objLetters);
+clearObj();           // очищаю объект от значений перед следующим поиском                                                          
+
+/* filter */
+function countLettersFilter(str){
+    const bufArray = Array.from(str.toString().toLowerCase());
+    const array = bufArray.filter(value => value in objLetters);
+    return array.length;
+}
+
+alert('Согласных букв: ' + countLettersFilter(prompt('Введите строку (filter)')));
+console.log(objLetters);
+clearObj();
+
+/* reduce */
+function countLettersReduce(str){
+    const bufArray = Array.from(str.toString().toLowerCase());
+    objLetters.sum = bufArray.reduce((acc, value) =>{
+        if(value in objLetters){
+            acc++;
+        }
+        return acc;
+    }, 0);
+
+    return objLetters.sum;
+}
+
+alert('Согласных букв: ' + countLettersReduce(prompt('Введите строку (reduce)')));
+console.log(objLetters);
+//clearObj();
+
+
+
+
+function clearObj(){
+    for(let value in objLetters){
+        objLetters[value] = 0;
+    }
+}
