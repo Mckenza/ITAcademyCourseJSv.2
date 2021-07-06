4c0ad2e5afb9f4716b6c11d0ac7cd62a77c9c7d6
Mckenza <Zhenya6207@gmail.com>
2021-07-06 19:38:52 +0300

    n13 homework

--- homework_4/n13_drinks_hash_func/drinks_hash.js
+++ homework_4/n13_drinks_hash_func/drinks_hash.js
@@ -8,8 +8,8 @@ function HashStorageFunc(){
     }
 
     this.getValue = function(key){
-        if(this.data[key]){
-            return -1;
+        if(!this.data[key]){
+            return false;
         } else {
             return this.data[key];
         }
@@ -26,10 +26,74 @@ function HashStorageFunc(){
 
     this.getKey = function(){
         const arrayKeys = Object.keys(this.data);
-        if(arrayKeys.length === 0){
+        if(arrayKeys.length !== 0){
             return arrayKeys;
         } else {
             return false;
         }
     }
+}
+
+const inputNameDrink = document.getElementById('input_name_id');
+const inputRecipe = document.getElementById('input_recipe_id');
+const checkAlco = document.getElementById('check_alcohol_id');
+const getInfoInput = document.getElementById('get_info_id');
+const showInfoDiv = document.getElementById('fullInfo_id');
+
+const logic = new HashStorageFunc();
+
+document.getElementById('add_button').onclick = () =>{
+    if(!inputNameDrink.value){
+        styleBoard(inputNameDrink);
+    } 
+    if (!inputRecipe.value){
+        styleBoard(inputRecipe);
+    } else {
+        const isAlco = checkAlco.checked;
+        logic.addValue(inputNameDrink.value, {recipe: inputRecipe.value, alco: isAlco});
+    }
+}
+
+document.getElementById('get_button').onclick = () =>{
+    if(!getInfoInput.value){
+        styleBoard(getInfoInput);
+    } else {
+        const item = logic.getValue(getInfoInput.value);
+        if(!item){
+            styleBoard(getInfoInput);
+        } else {
+            showInfoDiv.textContent = `Название: ${getInfoInput.value}
+            Рецепт: ${item.recipe}
+            Алкогольный? - ${item.alco ? 'Да' : 'Нет'}`;
+        }
+    }
+}
+
+document.getElementById('del_button').onclick = () =>{
+    if(!getInfoInput.value){
+        styleBoard(getInfoInput);
+    } else {
+        logic.deleteValue(getInfoInput.value);
+        showInfoDiv.textContent = '';
+    }
+}
+
+document.getElementById('all_list_button').onclick = () =>{
+    const arrayKeys = logic.getKey();
+    if(!arrayKeys){
+        showInfoDiv.textContent = 'нет элементов списка';
+    } else {
+        let str = '';
+        arrayKeys.forEach((value, index) =>{
+            str += `${(index + 1)}. ${value} `;
+        })
+        showInfoDiv.textContent = str;
+    }
+}
+
+function styleBoard(element){
+    setTimeout(()=>{
+        element.classList.remove('borderred');
+    }, 2000);
+    element.classList.add('borderred');
 }
